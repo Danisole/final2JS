7abe2a67d499b545a7e0b20bc9fab6b6fb3e19bb
danisole <danisole82@gmail.com>
2022-08-02 12:06:35 -0300

    arreglos de js

--- js/index.js
+++ js/index.js
@@ -10,6 +10,45 @@ window.addEventListener('scroll', e =>{
     logo.classList.toggle('active', window.scrollY > 0);
 });
 
+// const servicios = [];
+
+// let cantidad = 3;
+
+// do{
+//     let entrada = prompt("elija 3 opciones de servicios:\ndesayuno\nservicio de limpieza\nvista al lago\nmedia pension\nestacionamiento\nwifi")
+
+//     const serviciosContac = servicios.push(entrada.toLowerCase()) 
+   
+
+//     console.log(servicios.length)
+
+// }while(servicios.length != cantidad)
+
+// alert(`usted selecciono los siguientes servicios:\n${servicios.join("\n")}`)
+
+
+const carpas = [
+    {nombre: "carpa1", camas: 2, desayuno:true, precio: 2000},
+    {nombre: "carpa2", camas: 4, desayuno:true, precio: 2890},
+    {nombre: "carpa3", camas: 3, desayuno:true, precio: 2400},
+    {nombre: "cabaña", camas: 6, desayuno:true, precio: 4000},
+];
+
+let precioLimite = parseInt(prompt("ingrese la cantidad de camas que necesita"))
+
+let buscarPrecio = carpas.find((el) => el.precio < precioLimite)
+
+    if(buscarPrecio){
+        
+    alert(`segun su busqueda de camas disponemos de la habitacion ${buscarPrecio.nombre}`)
+
+    }else{
+
+        alert ("no hay coincidencias para su busqueda")
+        
+    }
+
+
 
 
 
