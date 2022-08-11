25c804f38c6c953195b1a8b1273b01eea96fd444
danisole <danisole82@gmail.com>
2022-08-11 02:52:32 -0300

    creacion de eventos

---
+++ js/carpas.js
@@ -0,0 +1,29 @@
+
+
+
+const carpas =[
+    {
+        nombre: "Carpa Sirirí",
+        precio: 2500,
+        descripcion:"carpa diseñada para 4 personas, cuenta con la mejor vista de la zona",
+        personas: 4
+    },
+    {
+        nombre: "Carpa Calandria",
+        precio: 2000,
+        descripcion:"carpa diseñada para 2 personas, cuenta con el mayor confort y privacidad",
+        personas: 2
+    },
+    {
+        nombre: "Carpa Muitú",
+        precio: 3200,
+        descripcion:"carpa diseñada para 5 personas, cuenta con cochera",
+        personas: 5
+    },
+    {
+        nombre: "Cabaña Ipacaá",
+        precio: 4800,
+        descripcion:"Cabaña diseñada para 7 personas, cuenta con el espacio y el confort necesario",
+        personas: 7
+    },
+];
--- js/index.js
+++ js/index.js
@@ -1,64 +1,57 @@
+const d = document,
+w = window;
 let title = document.getElementById('title');
 const nav = document.querySelector('.nav');
 const logo = document.querySelector('.logo');
+const botonParaSubir = document.querySelector("button");
+const nombreUsuario = document.getElementById("usuario")
 
 
-window.addEventListener('scroll', e =>{
-    let value = window.scrollY; 
+
+
+
+w.addEventListener('scroll', e =>{
+    let value = w.scrollY; 
     title.style.marginTop = value * .7 + 'px';
     nav.classList.toggle('active', window.scrollY > 0);
     logo.classList.toggle('active', window.scrollY > 0);
+
+});
+// agregar eventos
+
+botonParaSubir.addEventListener("click", ()=>{
+
+    window.scrollTo(0, 0);
+    
 });
 
-// const servicios = [];
+// saludo
 
-// let cantidad = 3;
+logo.addEventListener("click", saludo)
 
-// do{
-//     let entrada = prompt("elija 3 opciones de servicios:\ndesayuno\nservicio de limpieza\nvista al lago\nmedia pension\nestacionamiento\nwifi")
+function saludo(){
+    alert("Bienvenido a Aldea Glamping")
+}
 
-//     const serviciosContac = servicios.push(entrada.toLowerCase()) 
-   
+//logueo evento
 
-//     console.log(servicios.length)
+nombreUsuario.addEventListener("input", errorNombre)
 
-// }while(servicios.length != cantidad)
-
-// alert(`usted selecciono los siguientes servicios:\n${servicios.join("\n")}`)
-
-
-
-let nombre = [];
-
-let tuNombre = prompt("ingrese su nombre")
-
-let ingreseNombre = nombre.push(tuNombre.toLocaleUpperCase())
-
-alert(`Hola bienvenido ${nombre} a la pagina de reservas`)
+function errorNombre(){
+    if(isNaN(nombreUsuario.value)){
+        nombreUsuario.style.color="black"
+    }else{
+        nombreUsuario.style.color="red"
+    }
+}
 
 
 
 
-// const carpas = [
-//     {nombre: "carpa1", camas: 2, desayuno:true, precio: 2000},
-//     {nombre: "carpa2", camas: 4, desayuno:true, precio: 2890},
-//     {nombre: "carpa3", camas: 3, desayuno:true, precio: 2400},
-//     {nombre: "cabaña", camas: 6, desayuno:true, precio: 4000},
-// ];
 
-// let precioLimite = parseInt(prompt("Bienvenidos! ingrese la cantidad de camas que necesita"))
 
-// let buscarPrecio = carpas.find((el) => el.camas == precioLimite)
 
-//     if(buscarPrecio){
-        
-//     alert(`segun su busqueda de camas disponemos de la habitacion ${buscarPrecio.nombre}`)
 
-//     }else{
-
-//         alert ("no hay coincidencias para su busqueda")
-        
-//     }
 
 
 
